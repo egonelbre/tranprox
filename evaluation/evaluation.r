b10212bf5e60eca090a407ebec981d1dd1400306
Olga <olga.agen@gmail.com>
2012-12-11 22:40:17 +0200

    Edited evaluation.r

--- evaluation/evaluation.r
+++ evaluation/evaluation.r
@@ -6,37 +6,36 @@ library(rgl)
 
 run <- function(dataFile,customSeparator){
  read.table(dataFile, sep = customSeparator)
- #read.csv("http://www.ats.ucla.edu/stat/data/hsb2.csv")
+ #read.csv(dataFile)
 }
 
+##CASE SENSITIVE
 letterToVector <- function(letter){
 if(letter == "A"){x <- c(0,0,0) }
 else if (letter == "C"){x <- c(1,0,0)}
 else if (letter == "G"){x <- c(0,1,0)}
 else if (letter == "T"){x <- c(0,0,1)}
-x
 }
 
 createMatrix <- function(string){
-
 splitted <- strsplit(string, '')[[1]]
-	for(entry in splitted){
-	
-	}
-
+m <- matrix(0,length(splitted),3)
+for(i in 1:length(splitted)) {
+ m[i,] <- letterToVector(splitted[i]) 
+}
+m
 }
 
 evaluate <- function(data1, data2){
 
 }
 
+##fft - fourier transform
 transform <- function(data, func){
-	for(i in 1:nrow(data)) {
+apply(data,1,func)
+#	for(i in 1:nrow(data)) {
 #apply(data[i,],func)		
-
-
-	}
+#	}
 }
 
 
-for(i in 1:nrows(m)) {apply(m[i,],sum)	}
