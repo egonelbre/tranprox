0dcc60701c395a140a43543122d3ff7101373fa7
Egon Elbre <egonelbre@gmail.com>
2012-12-21 12:04:54 +0200

    proper interpolation

--- research/interpolation.r
+++ research/interpolation.r
@@ -1,121 +1,68 @@
-
-interpolate = function(x, n, kernel){
-  len = length(x)
-  apply(matrix(1:n), 1, function(i){ kernel(x, i*len/n) })
+clamp = function(value, low, high){
+  if(value < low) low
+  else if (value > high) high
+  else value
 }
 
-gaussian.left = function(x, i){
-  r = 1:length(x)
-  sum(dnorm(r, m=i) * x * (r <= i))
+interpolate3 = function(mat, n, kernel){
+  len = dim(mat)[2]
+  res = apply(matrix(1:n), 1, function(i){ kernel(mat, len, i, n) })
+  matrix(res, nrow=3, ncol=n)
 }
 
-gaussian.right = function(x, i){
-  r = 1:length(x)
-  sum(dnorm(r, m=i) * x * (r >= i))
+# kernels
+
+nearest = function(x, xlen, i, n){
+  xi = i*xlen/n
+  li = clamp(floor(xi), 1, xlen)
+  hi = clamp(ceiling(xi), 1, xlen)
+  cat(xi, ":", li, ":", hi, ":", i, ":", n, ":", xlen, "<\n")
+  if( li == hi ){
+    x[,li]
+  } else {
+    p = xi - li
+    matrix(x[,li]*(1-p) + x[,hi]*p)
+  }
 }
+
+linear = function(x, xlen, i, n){
+  xlow = floor((i-0.5)*xlen/n)
+  xhigh = ceiling((i+0.5)*xlen/n)
+  
+  li = clamp(xlow, 1, xlen)
+  hi = clamp(xhigh, 1, xlen)
+  count = hi - li + 1
+  
+  slice = matrix(mat[,li:hi], nrow=3)
+  rowSums(slice)/count
+}
+
 
 plot(sin(1:100/10))
-plot(interpolate(sin(1:100/10), 10, gaussian.left))
+plot(interpolate(sin(1:100/10), 10, linear))
 plot(interpolate(sin(1:100/10), 10, gaussian.right))
 
 plot(interpolate(1:100, 10, gaussian.right))
 
 sum(dnorm(c(1:10), m=3) * c(1:10))
 
-
-x <- 1:10
-y <- rnorm(10)
-par(mfrow = c(2,1))
-plot(x, y, main = "approx(.) and approxfun(.)")
-points(approx(x, y), col = 2, pch = "*")
-points(approx(x, y, method = "constant"), col = 4, pch = "*")
+plotLine = function(mat, col=1){
+  lines3d(mat[1,], mat[2,], mat[3,], col=col)
+}
 
 
-f <- approxfun(x, y)
-curve(f(x), 0, 11, col = "green2")
-points(x, y)
-is.function(fc <- approxfun(x, y, method = "const")) # TRUE
-curve(fc(x), 0, 10, col = "darkblue", add = TRUE)
-## different extrapolation on left and right side :
-plot(approxfun(x, y, rule = 2:1), 0, 11,
-     col = "tomato", add = TRUE, lty = 3, lwd = 2)
+open3d()
 
-## Show treatment of 'ties' :
+mat = rbind(sin(1:100/10), sin(2+1:100/3), sin(3+1:100/7))
+# original
+plotLine(mat, 2)
 
-x <- c(2,2:4,4,4,5,5,7,7,7)
-y <- c(1:6, 5:4, 3:1)
-approx(x, y, xout = x)$y # warning
-(ay <- approx(x, y, xout = x, ties = "ordered")$y)
-stopifnot(ay == c(2,2,3,6,6,6,4,4,1,1,1))
-approx(x, y, xout = x, ties = min)$y
-approx(x, y, xout = x, ties = max)$y
+# interpolated with linear
+mat2 = interpolate3(mat, 30, linear)
+plotLine(mat2, 3)
 
+# interpolated with nearest
+# mat3 = interpolate3(mat, 10, nearest)
+# plotLine(mat3, 4)
 
-
-require(graphics)
-
-op <- par(mfrow = c(2,1), mgp = c(2,.8,0), mar = .1+c(3,3,3,1))
-n <- 9
-x <- 1:n
-y <- rnorm(n)
-plot(x, y, main = paste("spline[fun](.) through", n, "points"))
-lines(spline(x, y))
-lines(spline(x, y, n = 201), col = 2)
-
-y <- (x-6)^2
-plot(x, y, main = "spline(.) -- 3 methods")
-lines(spline(x, y, n = 201), col = 2)
-lines(spline(x, y, n = 201, method = "natural"), col = 3)
-lines(spline(x, y, n = 201, method = "periodic"), col = 4)
-legend(6,25, c("fmm","natural","periodic"), col=2:4, lty=1)
-
-y <- sin((x-0.5)*pi)
-f <- splinefun(x, y)
-ls(envir = environment(f))
-splinecoef <- get("z", envir = environment(f))
-curve(f(x), 1, 10, col = "green", lwd = 1.5)
-points(splinecoef, col = "purple", cex = 2)
-curve(f(x, deriv=1), 1, 10, col = 2, lwd = 1.5)
-curve(f(x, deriv=2), 1, 10, col = 2, lwd = 1.5, n = 401)
-curve(f(x, deriv=3), 1, 10, col = 2, lwd = 1.5, n = 401)
-par(op)
-
-## Manual spline evaluation --- demo the coefficients :
-.x <- splinecoef$x
-u <- seq(3, 6, by = 0.25)
-(ii <- findInterval(u, .x))
-dx <- u - .x[ii]
-f.u <- with(splinecoef,
-            y[ii] + dx*(b[ii] + dx*(c[ii] + dx* d[ii])))
-stopifnot(all.equal(f(u), f.u))
-
-## An example with ties (non-unique  x values):
-set.seed(1); x <- round(rnorm(30), 1); y <- sin(pi * x) + rnorm(30)/10
-plot(x,y, main="spline(x,y)  when x has ties")
-lines(spline(x, y, n = 201), col = 2)
-## visualizes the non-unique ones:
-tx <- table(x); mx <- as.numeric(names(tx[tx > 1]))
-ry <- matrix(unlist(tapply(y, match(x, mx), range, simplify = FALSE)),
-             ncol = 2, byrow = TRUE)
-segments(mx, ry[, 1], mx, ry[, 2], col = "blue", lwd = 2)
-
-## An example of  monotone  interpolation
-n <- 20
-set.seed(11)
-x. <- sort(runif(n)) ; y. <- cumsum(abs(rnorm(n)))
-plot(x.,y.)
-curve(splinefun(x., y.)(x), add = TRUE, col = 2, n = 1001)
-curve(splinefun(x., y., method = "monoH.FC")(x), add = TRUE, col = 3, n = 1001)
-curve(splinefun(x., y., method = "hyman")(x), add = TRUE, col = 4, n = 1001)
-legend("topleft",
-       paste0("splinefun( \"", c("fmm", "monoH.FC", "hyman"), "\" )"),
-       col = 2:4, lty = 1)
-
-## and one from Fritsch and Carlson (1980), Dougherty et al (1989)
-x. <- c(7.09, 8.09, 8.19, 8.7, 9.2, 10, 12, 15, 20)
-f <- c(0, 2.76429e-5, 4.37498e-2, 0.169183, 0.469428, 0.943740,
-       0.998636, 0.999919, 0.999994)
-plot(x., f, ylim = c(-0.2, 1.2))
-curve(splinefun(x., f)(x), add = TRUE, col = 2, n = 1001)
-curve(splinefun(x., f, method = "hyman")(x), add = TRUE, col = 4, n = 1001)
-
+rgl.bringtotop()
