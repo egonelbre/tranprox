564706d683c7bf1519bf9d91762879ed95455103
Egon Elbre <egonelbre@gmail.com>
2012-12-31 16:18:35 +0200

    clustering

--- evaluation/clustering.r
+++ evaluation/clustering.r
@@ -22,7 +22,7 @@ axis(3, pos=6, seq(labels[1:100,]), labels[1:100,], las=2, cex.axis=0.7)
 data
 dist.comp
 
-par(mfrow=c(2,1))
+par(mfrow=c(1,1))
 
 distance = dist(dist.comp)
 cluster = hclust(distance, method="ward")
@@ -33,4 +33,23 @@ cluster = hclust(distance, method="ward")
 plot(cluster, hang=-1, label=data)
 
 
+colnames(dist.comp) = data
+rownames(dist.comp) = data
+
+colnames(dist.levenshtein) = data
+rownames(dist.levenshtein) = data
+
 library(igraph)
+
+plotgraph = function(dist){
+  temp = dist*(10-dist > 6)
+  g = graph.adjacency(temp, weighted=T, mode = "undirected")
+  g = simplify(g)
+  V(g)$label = V(g)$name
+  V(g)$degree = degree(g)
+  set.seed(3952)
+  layout1 = layout.fruchterman.reingold(g)
+  plot(g, layout=layout1)  
+}
+
+plotgraph(dist.comp)
