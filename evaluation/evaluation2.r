64accac5bba3dd78b74623160db08dd5b5fc91bc
Egon Elbre <egonelbre@gmail.com>
2013-01-04 19:14:20 +0200

    writing report

--- evaluation/evaluation2.r
+++ evaluation/evaluation2.r
@@ -10,12 +10,17 @@ dist.levenshtein = run(data, compose(), levenshtein)
 dist.none = run(data, compose(convertToMatrix), euclidean)
 dist.blur = run(data, compose(convertToMatrix, mkBlur()), euclidean)
 dist.fourier = run(data, compose(convertToMatrix, fourier), euclideanIm)
+dist.fourierM = run(data, compose(convertToMatrix, fourier), manhattan)
+dist.fourierRe = run(data, compose(convertToMatrix, fourier, Re), euclidean)
+dist.fourierIm = run(data, compose(convertToMatrix, fourier, Im), euclidean)
 dist.haar = run(data, compose(convertToMatrix, haar), euclidean)
 
-matplot(cbind(dist.levenshtein, dist.none, dist.blur, dist.fourier, dist.haar)[1:50,], type="l", axes=FALSE)
+matplot(cbind(dist.levenshtein, dist.none, dist.blur, dist.fourierRe, dist.fourierIm, dist.haar)[1:50,], type="l", axes=FALSE)
+
+matplot(cbind(dist.levenshtein, dist.none, dist.fourierRe, dist.fourierIm, dist.fourier*1.5, dist.fourierM/2)[1:50,], type="l", axes=FALSE)
 axis(2)
-axis(3, pos=6, seq(labels), labels, las=2, cex.axis=0.7)
-legend(0,4, c("Levenshtein", "None", "Blur", "Fourier", "Haar"), fill=1:6)
+#axis(3, pos=6, seq(labels), labels, las=2, cex.axis=0.7)
+legend(2,4, c("Levenshtein", "None", "FourierRe", "FourierIm", "Fourier", "FourierM/2"), fill=1:6, cex=0.7)
 
 lm(dist.levenshtein ~ dist.none + dist.haar)
 
@@ -26,12 +31,13 @@ lm(dist.fourier ~ dist.none)
 
 dist.comp = ceiling(floor(dist.blur) * 1.2 + frac(dist.blur) * 1.25 + 0.3481)
 matplot(cbind(dist.levenshtein, dist.none, dist.comp)[1:50,], type="l", axes=FALSE)
-
-matplot(cbind(dist.levenshtein, dist.none, ceiling(dist.blur) + 1)[1:50,], type="l", axes=FALSE)
+matplot(cbind(dist.levenshtein, dist.none, floor(dist.blur) + 2)[1:50,], type="l", axes=FALSE)
 
 dist.comp = (dist.fourier/12 + dist.haar)/1.6
 
-matplot(cbind(dist.levenshtein, dist.none * 0.6082 - dist.fourier*0.0 - dist.haar*0.2613)[1:50,], type="l", axes=FALSE)
+dist = cbind(dist.levenshtein, dist.none * 0.6082 - dist.fourier*0.0 - dist.haar*0.2613)
+colnames(dist) = labels
+matplot(dist[1:50,], type="l", axes=T)
 axis(2)
 axis(3, pos=6, seq(labels[1:100,]), labels[1:100,], las=2, cex.axis=0.7)
 
